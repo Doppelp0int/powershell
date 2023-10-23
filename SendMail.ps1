9401f60eb6e93f3730f5379a4ad6857375ce6410
Doppelp0int <148204656+Doppelp0int@users.noreply.github.com>
2023-10-23 10:19:52 +0200

    Update SendMail.ps1

--- SendMail.ps1
+++ SendMail.ps1
@@ -1,10 +1,10 @@
-$EmailTo = "davidstebner@web.de" 
-$EmailFrom = "node@gartenkriege.de"
+$EmailTo = "SENDTO@web.de" 
+$EmailFrom = "FROM@gmx.de"
 $Subject = "Monitoring Error" 
 $Body = "Test Email"  
-$SMTPServer = "shared02.keymachine.de" 
+$SMTPServer = "SMTPSERVER" 
 $SMTPMessage = New-Object System.Net.Mail.MailMessage($EmailFrom,$EmailTo,$Subject,$Body)
 $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) 
 $SMTPClient.EnableSsl = $true 
-$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("doppel@gartenkriege.de", "****");
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("EMAILFROM", "PASSWORDFROM");
 $SMTPClient.Send($SMTPMessage)
