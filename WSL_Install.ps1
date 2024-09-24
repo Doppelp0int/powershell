7b20bc772c5c8a865e05dd8f4f9dc08739b1971f
Doppelp0int <148204656+Doppelp0int@users.noreply.github.com>
2024-09-24 16:18:08 +0200

    Update WSL_Install.ps1

--- WSL_Install.ps1
+++ WSL_Install.ps1
@@ -3,6 +3,18 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit
 }
 
+Write-Host "`n"
+Write-Host "                    -------------------------------------------------            " -ForegroundColor Cyan
+Write-Host "                    |                                               |            " -ForegroundColor Cyan
+Write-Host "                    |               WSL Installer                   |            " -ForegroundColor Cyan
+Write-Host "                    |          Version 1.4 -> 24.09.2024            |            " -ForegroundColor Cyan
+Write-Host "                    |                                               |            " -ForegroundColor Cyan
+Write-Host "                    |                                               |            " -ForegroundColor Cyan
+Write-Host "                    -------------------------------------------------            " -ForegroundColor Cyan
+Write-Host "`n"
+
+
+
 # Funktion zum Prüfen, ob WSL aktiviert ist
 function Check-WSL-Installation {
     $wslFeature = Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
@@ -13,23 +25,18 @@ function Check-WSL-Installation {
         Install-WSL
     }
 }
-
-
 # Funktion zur deinstallation bei problemen.
 function Uninstall-WSL{
-Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
-Disable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
+	Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+	Disable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
     wsl --uninstall
     wsl --unregister ubuntu
 }
-
 # Funktion zur Installation von WSL
 function Install-WSL {
     Write-Host "INFO: WSL wird installiert..." -Fore Yellow
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All -NoRestart
-Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All -NoRestart
-
-
+	Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All -NoRestart
+	Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All -NoRestart
     # Setze WSL 2 als Standard
         Start-Process "wsl.exe" -ArgumentList "--set-default-version 2" -NoNewWindow
             Write-Host "INFO: WSL wird aktuallisiert..." -Fore Yellow
@@ -38,53 +45,50 @@ Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All -
 
     Write-Host "INFO: WSL wurde erfolgreich installiert." -Fore Green
 }
-
 # Funktion zur Installation von Ubuntu ohne Benutzererstellung
 function Install-Ubuntu {
-
     Write-Host "INFO: Ubuntu wird installiert... bitte warten..." -Fore Yellow
 
-    Start-Process "wsl.exe" -ArgumentList "--install --web-download -d Ubuntu " -NoNewWindow
+    Start-Process "wsl.exe" -ArgumentList "--install --web-download -d Ubuntu" -NoNewWindow
     Start-Sleep -Seconds 5
     wsl --terminate "Ubuntu"
+
     # Warten, bis Ubuntu installiert wurde
-    Start-Sleep -Seconds 5
+    Write-Host "INFO: Warte auf die Installation von Ubuntu..." -Fore Yellow
 
-
+    # Prüfen, ob die Ubuntu-Instanz verfügbar ist
     do {
-    # Befehl ausführen
-    $output = wsl -l | Where-Object { $_.Replace("`0","") -match '^Ubuntu' }
-    
-    # Prüfen, ob ein Ergebnis vorhanden ist
-    if ($output) {
-        Write-Host "Ubuntu gefunden. Skript wird fortgesetzt."
-        break # Schleife verlassen, wenn Bedingung erfüllt ist
-    }
-    
-    # Warten für 3 Sekunden
-    Start-Sleep -Seconds 5
+        Start-Sleep -Seconds 3
+        
+        # Befehl ausführen
+        $output = wsl -l | Where-Object { $_ -replace "`0","" -match '^Ubuntu' }
+        
+        # Prüfen, ob ein Ergebnis vorhanden ist
+        if ($output) {
+            Write-Host "INFO: Ubuntu gefunden. Skript wird fortgesetzt." -Fore Green
+            break # Schleife verlassen, wenn Bedingung erfüllt ist
+        } else {
+            Write-Host "INFO: Warten auf Ubuntu-Installation..." -Fore Yellow
+        }
+    } while ($true)
 
-} while ($true)
     # Download des Installationsskripts von GitHub und Ausführung
     $scriptUrl = "https://raw.githubusercontent.com/Doppelp0int/batch/refs/heads/main/WSL_install_Docker.sh"
     $outputPath = "C:\temp\WSL_install_Docker.sh"
     Write-Host "INFO: Download des Installationsskripts von GitHub..." -Fore Yellow
     Invoke-WebRequest -Uri $scriptUrl -OutFile $outputPath
-
     # Führe das Skript in der WSL-Umgebung aus
-    #$folder = "/mnt/c/temp" # Pfad in der WSL-Umgebung
-    #wsl bash -c "bash $outputPath"
     wsl -u root bash -c "cd /mnt/c/temp && bash ./WSL_install_Docker.sh"
     Write-Host "INFO: Installationsskript erfolgreich ausgeführt." -Fore Green
-
     # Bereinige das Skript nach der Ausführung
     try {
         Remove-Item -Path $outputPath -Force
+        Write-Host "INFO: Skript erfolgreich gelöscht." -Fore Green
+		Start-Sleep -Seconds 1
     } catch {
         Write-Host "ERROR: Fehler beim Löschen des Skripts $outputPath." -Fore Red
     }
 }
-
 # Hauptlogik des Skripts
 try {
     # Überprüfen, ob WSL installiert ist
@@ -97,7 +101,7 @@ try {
     # Überprüfen, ob WSL ordnungsgemäß funktioniert
     try {
         wsl.exe --list --online | Out-Null
-        Write-Host "CHECK: WSL funktioniert einwandfrei." -Fore Green
+        Write-Host "CHECK: WSL funktioniert einwandfrei!" -Fore Yellow
     } catch {
         Write-Host "CHECK: Problem mit WSL erkannt. Deinstallation wird durchgeführt." -Fore Red
         Uninstall-WSL
@@ -106,12 +110,16 @@ try {
 
     # Installiere und konfiguriere Ubuntu
     Install-Ubuntu
-    Start-Sleep -Seconds 2
-    Write-Host "INFO: Ubuntu wurde erfolgreich installiert und eingerichtet." -Fore Green
-    # Öffne den Portainer-Link
-    Write-Host "READY: Öffne Portainer...https://localhost:9443/#!/init/admin -Installer wird geschlossen......" -Fore Green
-    Start-Process "https://localhost:9443/#!/init/admin"
-    Start-Sleep -Seconds 5
+	cls
+	Write-Host ""
+	Write-Host ""
+	Write-Host ""
+	Start-Sleep -Seconds 1
+    Write-Host "READY!: Ubuntu wurde erfolgreich installiert und eingerichtet." -Fore Green
+	Start-Sleep -Seconds 1
+    Write-Host "READY: Öffne Portainer...https://localhost:9443/#!/init/admin - Installer wird geschlossen......" -Fore Green
+    Start-Process cmd -ArgumentList '/c','start https://localhost:9443/#!/init/admin' # auto close
+    Start-Sleep -Seconds 10
     Exit
 } catch {
     Write-Host "ERROR: Ein unerwarteter Fehler ist aufgetreten." -Fore Red
