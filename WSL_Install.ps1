831a03e8e3863af79cc44e1691fc3c08ad32e514
Doppelp0int <148204656+Doppelp0int@users.noreply.github.com>
2024-09-24 16:19:27 +0200

    Update WSL_Install.ps1

--- WSL_Install.ps1
+++ WSL_Install.ps1
@@ -2,7 +2,10 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit
 }
-
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
 Write-Host "`n"
 Write-Host "                    -------------------------------------------------            " -ForegroundColor Cyan
 Write-Host "                    |                                               |            " -ForegroundColor Cyan
